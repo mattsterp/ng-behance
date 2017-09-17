3516730392d8d102099bb0be4682b0e608133de6
Matthew Peters <matt@mattsterp.com>
2017-09-17 15:27:32 -0400

    Added foundation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule
